f9ad0b3c8d3cb5835b58b451540734fbcca25ca1
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2020-03-02 19:41:43 +0100

    Async all the things
    Using axios instead of request which was deprecated in february 2020

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const cheerio = require('cheerio')
-const request = require('request')
+const axios = require('axios')
 
 function cleanupString(str) {
   return str.replace("\n", '').trim()
@@ -10,26 +10,34 @@ function cleanupDate(str) {
 
 const organizerPageURL = 'https://www.eventbrite.it/o/torino-hacknight-9053329779'
 
-request(organizerPageURL, function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    // console.log(html)
-    const $ = cheerio.load(html)
-
-    const selector = "#live_events"
-    let events = []
-
-    $(".list-card-v2", selector).each((idx, e) => {
-      // console.log(e)
-      events.push({
-        title: cleanupString($('.list-card__title', $(e)).text()),
-        date: cleanupDate($('.list-card__date', $(e)).text()),
-        venue: cleanupString($('.list-card__venue', $(e)).text()),
-        imageURL: $('.list-card__image img').attr('src'),
-        url: $('a', $(e)).attr('href'),
-        eid: "eventbrite:" + $('a', $(e)).attr('data-eid'),
-      })
-    })
-
-    console.log(events)
+async function getOrganizerEvents(url) {
+  let response
+  try {
+    response = await axios.get(url)
+  } catch(error) {
+    console.error(error)
+    return
   }
+
+  const $ = cheerio.load(response.data)
+  const selector = "#live_events"
+  let events = []
+
+  $(".list-card-v2", selector).each((idx, e) => {
+    // console.log(e)
+    events.push({
+      title: cleanupString($('.list-card__title', $(e)).text()),
+      date: cleanupDate($('.list-card__date', $(e)).text()),
+      venue: cleanupString($('.list-card__venue', $(e)).text()),
+      imageURL: $('.list-card__image img').attr('src'),
+      url: $('a', $(e)).attr('href'),
+      eid: "eventbrite:" + $('a', $(e)).attr('data-eid'),
+    })
+  })
+
+  return events
+}
+
+getOrganizerEvents(organizerPageURL).then((events) => {
+  console.log(events)
 })
